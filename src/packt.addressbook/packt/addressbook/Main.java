54fc4c58a7980da8f09817723c811904537ca5cb
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-09-26 15:14:48 +0200

    New module created - packt.contact. Model Contact moved to it, contacts now are loading from assets/input.xml file

--- src/packt.addressbook/packt/addressbook/Main.java
+++ src/packt.addressbook/packt/addressbook/Main.java
@@ -1,8 +1,10 @@
 package packt.addressbook;
 
-import packt.addressbook.model.Contact;
-import packt.addressbook.util.ContactUtil;
+import packt.contact.model.Contact;
+import packt.contact.util.ContactLoader;
+import packt.contact.util.ContactLoadException;
 import packt.util.SortUtil;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import java.util.List;
@@ -10,14 +12,23 @@ import java.util.List;
 public class Main {
 	private static final Logger logger = Logger.getLogger(Main.class.getName());
 	
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		logger.info("Address book viewer application: Started");
-        ContactUtil contactUtil = new ContactUtil();
-        SortUtil sortUtil = new SortUtil();
-        List<Contact> contacts = contactUtil.getContacts();
-        sortUtil.sortList(contacts);
-        System.out.println(contacts);
+		
+		List<Contact> contacts = new ArrayList<>();
+		ContactLoader contactLoader = new ContactLoader();
+		SortUtil sortUtil = new SortUtil();
+		try {
+			contacts = contactLoader.loadContacts("assets/input.xml");
+		} catch (ContactLoadException e) {
+			logger.severe(e.getMessage());
+			System.exit(0);
+		}
+		sortUtil.sortList(contacts);
+		
+		System.out.println(contacts);
+		
 		logger.info("Address book viewer application: Completed");
-    }
+	}
 }
 
