4e28d14dcdcd2acdc2ef7dcb28891f5b388eda14
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-09-30 06:53:03 +0200

    Graphical UI added.

--- src/packt.contact/packt/contact/util/ContactLoader.java
+++ src/packt.contact/packt/contact/util/ContactLoader.java
@@ -11,29 +11,44 @@ import org.w3c.dom.Node;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 import java.io.IOException;
+import packt.contact.model.Address;
 
 public class ContactLoader {
-	public List<Contact> loadContacts(String fileName) throws ContactLoadException{
-		
+	
+	public List<Contact> parseXml(String fileName) throws ContactLoadException {
+
 		List<Contact> contacts = new ArrayList<>();
-		
 		XmlUtil xmlUtil = new XmlUtil();
+		
+		
 		Document doc;
 		try {
 			doc = xmlUtil.loadXmlFile(fileName);
-			
-			NodeList nList = doc.getElementsByTagName("contact");
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node node = nList.item(temp);
-				if (node.getNodeType() == Node.ELEMENT_NODE) {
-				Element eElement = (Element) node;
-				Contact contact = new Contact("id", eElement.getElementsByTagName("firstname").item(0).getTextContent(), eElement.getElementsByTagName("lastname").item(0).getTextContent(), "000-000-000");
-				contacts.add(contact);
-			 }
-		  }
-		  return contacts;
 		} catch (ParserConfigurationException | SAXException | IOException e) {
 			throw new ContactLoadException("Unable to load Contact file");
 		}
+		NodeList nList = doc.getElementsByTagName("contact");
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node contactNode = nList.item(temp);
+			if (contactNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) contactNode;
+
+				Contact contact = new Contact();
+				contact.setFirstName(xmlUtil.getElement(contactNode, "firstname"));
+				contact.setLastName(xmlUtil.getElement(contactNode, "lastname"));
+				contact.setPhone(xmlUtil.getElement(contactNode, "phone"));
+				Node addressNode = eElement.getElementsByTagName("address").item(0);
+
+				Address address = new Address();
+				address.setStreet(xmlUtil.getElement(addressNode, "street"));
+				address.setCity(xmlUtil.getElement(addressNode, "city"));
+				address.setState(xmlUtil.getElement(addressNode, "state"));
+				address.setCountry(xmlUtil.getElement(addressNode, "country"));
+				contact.setAddress(address);
+
+				contacts.add(contact);
+			}
+		}
+		return contacts;
 	}
 }
